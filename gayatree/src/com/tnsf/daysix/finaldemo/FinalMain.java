20a17bd691c932db95e1b94d86bbd88f2a2b68f1
GAYATRI <GAYATRI@LAPTOP-LHTCK2MG>
2025-07-07 10:21:00 +0530

    daysix

---
+++ gayatree/src/com/tnsf/daysix/finaldemo/FinalMain.java
@@ -0,0 +1,13 @@
+package com.tnsf.daysix.finaldemo;
+
+public class FinalMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FinalTestDemo f=new FinalTestDemo();
+		f.display();
+		f.show();
+	}
+
+}
